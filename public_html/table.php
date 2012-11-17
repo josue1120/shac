58e44892f1dc7cd60f56f1ac276464e92bd4d38c
Josue Daniel Bustamante <josue1120@outlook.com>
2012-11-16 19:54:24 -0500

    Primera subid, primer desarrollo

---
+++ public_html/table.php
@@ -0,0 +1,20 @@
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Dia</th>
+            <th>Asignaturas</th>
+            <th>Actividades</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td rowspan="2">15/11/2012</td>
+            <td>Sociales</td>
+            <td>...</td>
+        </tr>
+        <tr>
+            <td>Matematicas</td>
+            <td>...</td>
+        </tr>
+    </tbody>
+</table>
